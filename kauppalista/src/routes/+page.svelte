2b9355bacdaad93d251ef736edcee48b69884c71
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2024-04-21 11:06:54 +0300

    kauppalista page toimii alustavasti

--- kauppalista/src/routes/+page.svelte
+++ kauppalista/src/routes/+page.svelte
@@ -1,16 +1,66 @@
 <script>
-    const jutut = [
-        "Leipä",
-        "Tomaatti",
-        "Kurkku"
-    ];
+    import {enhance} from '$app/forms'
+
+    export let data;
+    export let form;   
 </script>
 
-<h1>Kauppalista</h1>
+<div class="komponentti">
+    <h1>Kauppalista</h1>
 
-<ul>
-    {#each jutut as juttu}
-    <li>{juttu}</li>
-    {/each}
-</ul>
+    <ul>
+        {#each data.asiat as asia}
+        <li>
+            <input type="checkbox" />
+            {asia}
+        </li>
+        {/each}  
+    </ul>
+    {#if form?.error}
+    <p class="error">{form.error}</p>
+    {/if}
+    <form class="uusi" method="POST" action="?/lisääAsia" use:enhance>
+        <label for="uusi-asia">Lisää uusi tuote:</label>
+        <!-- svelte-ignore a11y-autofocus -->
+        <input 
+            id="uusi-asia" 
+            name="asia" 
+            type="text" 
+            value={form?.asia ?? ''}
+            required 
+            autofocus/>
+        <button>Lisää</button>
+    </form>
+</div>
+
+<style>
+    .komponentti {
+        background-color: orange;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        font-size: 20px;
+    }
+    h1 {
+        color: green;
+        font-size: 200%;
+    }
+    ul {
+        color: greenyellow;
+        font-size: 150%;
+        background-color: orangered;
+    }
+    li {
+        list-style-type: none;
+        
+    }
+    .uusi {
+        background-color: orange;
+        font-size: 150%;
+    }
+    label {
+        display: block;
+    }
+    
+
+</style>
+
 
