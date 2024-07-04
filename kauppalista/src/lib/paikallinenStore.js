d15f37045efaa0cab77303394bec7e24567eed07
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2024-07-04 18:25:56 +0300

    toimii local storessa

---
+++ kauppalista/src/lib/paikallinenStore.js
@@ -0,0 +1,24 @@
+import { writable } from 'svelte/store';
+
+export function paikallinenStore(avaimenNimi, alkuArvo) {
+    const tallennettuArvo =
+        typeof localStorage !== 'undefined' 
+            ? localStorage?.getItem(avaimenNimi)
+            : undefined;
+    let tallennettuTaiAlkuArvo = alkuArvo;
+    if (tallennettuArvo) {
+        tallennettuTaiAlkuArvo = JSON.parse(tallennettuArvo);
+    }
+
+    const taustaStore = writable(tallennettuTaiAlkuArvo);
+
+    return {
+        subscribe: taustaStore.subscribe,
+        update: taustaStore.update,
+        set: (arvo) => {
+         console.log('Set kutsuttu arvolla:', arvo);
+         localStorage.setItem(avaimenNimi, JSON.stringify(arvo));
+         taustaStore.set(arvo);
+        },
+    };
+}
