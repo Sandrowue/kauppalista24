3f88e28be8ba3ab08b88f9983dfccbac5d23c641
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2024-07-19 13:59:31 +0300

    kuva lisätty

--- kauppalista/src/routes/+page.svelte
+++ kauppalista/src/routes/+page.svelte
@@ -1,3 +1,25 @@
+<script>
+    import tunnelmakuva from './tara-clark-IpbzCbvVA7Q-unsplash.jpg';
+</script>
+
 <h2>Etusivu</h2>
 
-<a href="/listat">Listat</a>
+<figure>
+    <a href="/listat">Listat
+        <img src={tunnelmakuva} alt="Ostoskori">
+    </a>
+    <figcaption>
+        Kuva:
+        <a href="https://unsplash.com/@socialtyvr">Tara Clark</a>
+    </figcaption>
+</figure>
+
+<style> 
+  
+    figcaption {
+        text-align: right;
+        font-size: 0.75rem;
+        padding-top: 0;
+        padding-right: var(--pico-border-radius);
+    }
+</style>
